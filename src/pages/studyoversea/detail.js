6b461e942ffa5160685d73c3de514c50acc4b19f
rico <ricardocao.biker@gmail.com>
2023-07-30 00:04:53 +0800

    fix

--- src/pages/studyoversea/detail.js
+++ src/pages/studyoversea/detail.js
@@ -45,11 +45,11 @@ const Detail = () => {
                 {data.school_name}
               </div>
               <div className="flex gap-5">
-                <div className="flex justify-center items-center">
-                  <img src={data.logo_url} width="180" />
-                </div>
                 <div>
                   <div className="p-5">
+                    <div>
+                      <img src={data.logo_url} width="220" />
+                    </div>
                     <div className="flex items-center gap-5 pb-5 border-b">
                       <img src={data.flag_url} width="50" height="20" />
                       <span className="text-lg font-bold">
@@ -76,11 +76,11 @@ const Detail = () => {
                   <img src={data.school_image_url} />
                 </div>
               </div>
-              <div className="text-3xl fotn-bold pt-5 main-color">{t('overview')}</div>
+              <div className="text-3xl fotn-bold pt-5 main-color">{t('overview')}: {data.school_name}</div>
               <div className="info-graph pt-5 leading-7" dangerouslySetInnerHTML={{__html: data.school_info}}></div>
               <div className="flex gap-5 mb-10">
                 <div className="w-1/2"><img width="w-full" src={data.city_image_url} /></div>
-                <div className="w-1/2"><div className="text-3xl fotn-bold pt-5 main-color">{t('location')}</div>
+                <div className="w-1/2"><div className="text-3xl fotn-bold pt-5 main-color">{t('location')}:{data.city}</div>
                   <div className="info-graph pt-5 leading-7" dangerouslySetInnerHTML={{__html: data.city_info}}></div></div>
               </div>
             </div>
