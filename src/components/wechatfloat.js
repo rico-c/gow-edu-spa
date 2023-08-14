4084faee8eef634c384162edaccd9671b848d902
rico <ricardocao.biker@gmail.com>
2023-08-14 11:54:01 +0800

    x

--- src/components/wechatfloat.js
+++ src/components/wechatfloat.js
@@ -2,4 +2,13 @@ import React from 'react';
 import {FloatButton, Popover, QRCode} from 'antd';
 import {WechatOutlined} from '@ant-design/icons';
 
-export const WechatFloat = () => <Popover placement="leftBottom" content={<img width="200px" src="/qr/wechat.jpeg" />} trigger="click"><FloatButton onClick={() => console.log('click')} icon={<WechatOutlined style={{fontSize: '25px', transform: 'translateX(-3px)'}} />} /></Popover>
+export const WechatFloat = () => <Popover
+  className="right-3 bottom-20 md:bottom-24 md:right-6"
+  placement="leftBottom"
+  content={<img width="200px" src="/qr/wechat.jpeg" />} trigger="click">
+  <FloatButton
+    onClick={() => console.log('click')}
+    icon={<WechatOutlined style={{fontSize: '25px', transform: 'translateX(-3px)'}}
+    />}
+  />
+</Popover>
